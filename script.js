83918a9bc384b56542d327c5e870524b0c40b5e9
iamsamirtagiyev <stagiyev579@gmail.com>
2023-11-01 19:33:19 +0400

    bmi

---
+++ script.js
@@ -0,0 +1,33 @@
+// ------------> Part 1 <------------
+
+let nihadBoy1 = 1.69;
+let nihadCeki1 = 78;
+let nailBoy1 = 1.95;
+let nailCeki1 = 92;
+
+nihad1 = nihadCeki1 / (nihadBoy1 ** 2);
+nail1 = nailCeki1 / (nailBoy1 ** 2);
+
+if(nihad1 > nail1){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+// ------------> Part 2 <------------
+
+let nihadBoy2 = 1.88;
+let nihadCeki2 = 95;
+let nailBoy2 = 1.76;
+let nailCeki2 = 85;
+
+nihad2 = nihadCeki2 / (nihadBoy2 ** 2);
+nail2 = nailCeki2 / (nailBoy2 ** 2);
+
+if(nihad2 > nail2){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
